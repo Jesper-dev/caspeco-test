66f5e4630a5d9fe5111007edcc6ba4e9e1cda50a
JesperKYH <jesper.pettersson@student.kyh.se>
2021-05-18 09:47:11 +0200

    The color persists after refresh

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import './App.css';
 import ComponentA from './components/componentA';
 import ComponentB from './components/componentB';
 import { PageContainer } from './StyledComponents'
 
 function App() {
+  let bgA = localStorage.getItem('backgroundA')
+  let bgB = localStorage.getItem('backgroundB')
   const [changeColorA, setChangeColorA] = useState(false)
   const [changeColorB, setChangeColorB] = useState(false)
+  useEffect(() => {
+
+    if(bgA === 'true') {
+      setChangeColorA(true)
+    } else if(bgB === 'true') {
+      setChangeColorB(true)
+    }
+    console.log('bgA is: ', bgA)
+    console.log('bgB is: ', typeof bgB)
+  }, [])
+
+
   const changeColorFunc = (e) => {
     if(e.target.textContent === 'Change Color A') {
       setChangeColorA(!changeColorA)
       setChangeColorB(false)
+      localStorage.setItem('backgroundA', !changeColorA)
+      localStorage.setItem('backgroundB', false)
     } else {
       setChangeColorB(!changeColorB)
       setChangeColorA(false)
+      localStorage.setItem('backgroundA', false)
+      localStorage.setItem('backgroundB', !changeColorB)
     }
 
   }
--- src/components/componentA/index.js
+++ src/components/componentA/index.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import { Wrapper } from '../../StyledComponents'
 
 const ComponentA = ({changeColorFunc, changeColorValue}) => {
-    console.log(changeColorValue, 'In CompA')
     return (
         <Wrapper changeColor={changeColorValue}>
             <span className={"component_title"}> Component A </span>
